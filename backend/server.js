c8de3d5335ca673cf671b5402288d0f096c31dd2
santiChiave <santichiave@gmail.com>
2020-09-21 20:15:18 -0300

    a

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,19 @@ const cors = require('cors')
 require ("./config/cnx-db")
 const app = express()
 const router = require('./routes');
+
 /* const router = require('./routes/routes') */
 
 
 const port = process.env.PORT
-
 const host = process.env.HOST || '0.0.0.0'
 
-
+app.use('/uploads', express.static(`${__dirname}/uploads`))
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload())
+app.use("/api", router)
 
-/* app.use('/api', router)  */
 
 
 app.listen(port, host, ( ) => console.log("Listening on PORT "  + port))
