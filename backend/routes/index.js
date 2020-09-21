0c4d42df15b81000e826cd0f60bdddbfa2711d83
santiChiave <santichiave@gmail.com>
2020-09-21 19:26:20 -0300

    backend: usuarios + backend Lari

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,21 @@
 const express = require("express")
 const itemsController = require("../controllers/itemsController")
+const usuariosController = require("../controllers/usuariosController")
 const router = express.Router()
+
+
+
+router.route('/usuario')
+.post(usuariosController.crearCuenta)
+
+router.route('/usuarioGoogle')
+.post(usuariosController.crearCuentaConGoogle)
+
+router.route('/login')
+.post(usuariosController.loguearUsuario)
+
+router.route('/modificarUsuario')
+.put(usuariosController.modificarUsuario)
 
 router.route("/items")
 .get(itemsController.getProducts)
