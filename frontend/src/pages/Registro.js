03c805cde42bafeea50dceb4aac1dfacf0021bbd
EddyVega-dev <evegag@fi.uba.ar>
2020-09-22 17:30:15 -0300

    modificación signup

--- frontend/src/pages/Registro.js
+++ frontend/src/pages/Registro.js
@@ -66,7 +66,7 @@ const Registro = (props) => {
             if (!response.success) {
                 if (response.usuario !== ""){
                     setErrors({
-                        ..,errors,
+                        ...errors,
                         user: response.usuario
                     })
                 }
