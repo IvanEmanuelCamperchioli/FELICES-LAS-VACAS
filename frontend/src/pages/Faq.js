59b6e62d3ebb651ebea3cf985ccc08c31cf2764e
IvanEmanuelCamperchioli <ivanemanuel2137@gmail.com>
2020-09-30 13:49:44 -0300

    cambios al faq page

--- frontend/src/pages/Faq.js
+++ frontend/src/pages/Faq.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import '../styles/faq.css'
 import Header from '../components/Header'
+import Footer from '../components/Footer';
 
 class Faq extends React.Component {
 
@@ -53,12 +54,13 @@ class Faq extends React.Component {
         return (
             <>
             <Header />
-            <h1>Preguntas frecuentes</h1>
+            <div className="fondo-faq">
+            <h1 className="titulo-faq-1">Preguntas frecuentes</h1>
                 <div className="PreguntasFrecuentes">
                     <Button onClick={() => this.toggle(1)} className="button-Faq" >¿Por qué ser vegano?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.one.isOpen}>
                         <Card className="card-faq">
-                            <CardBody style={{width: '100%'}}>
+                            <CardBody className="card-body">
                                 <p> Porque el veganismo hace sentir consecuentes el amor y respeto que tenemos 
                                     hacia los seres vivos y el planeta.
                                     Consideramos que el futuro es vegano, por eso lucharemos para activar 
@@ -69,19 +71,19 @@ class Faq extends React.Component {
                     <Button onClick={() => this.toggle(2)} className="button-Faq" >¿Cuál es un producto vegano?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.two.isOpen}>
                         <Card className="card-faq">
-                            <CardBody>
+                            <CardBody className="card-body">
                                 <p>Se considera vegano todo aquel producto cuyos ingredientes 
                                     y procesos no provengan de animales ni de sus derivados.</p>                        
                             </CardBody>
                         </Card>
                     </Collapse>
                 </div>
-            <h1>Formas de pago</h1>
+            <h1 className="titulo-faq">Formas de pago</h1>
             <div className="formasDePago">
                     <Button onClick={() => this.toggle(3)} className="button-Faq" >¿Cuáles son los medios de pago?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.three.isOpen}>
                         <Card className="card-faq">
-                            <CardBody style={{width: '100%'}}>
+                            <CardBody className="card-body">
                                 <h4>Dinero en Mercado Pago</h4>
                                 <h4>Tarjeta de crédito en hasta 12 cuotas</h4>
                                 <h4>Tarjeta de débito</h4>
@@ -92,7 +94,7 @@ class Faq extends React.Component {
                     <Button onClick={() => this.toggle(4)} className="button-Faq" >¿Es segura mi compra con mercado pago?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.four.isOpen}>
                         <Card className="card-faq">
-                            <CardBody>
+                            <CardBody className="card-body">
                                 <p> MercadoPago es la plataforma de pago online líder de América Latina y 
                                     garantiza la seguridad en todas las operaciones que se generen a través de su sistema.
                                     Cuenta con un sistema de privacidad que protege los datos de la tarjeta y 
@@ -102,21 +104,21 @@ class Faq extends React.Component {
                         </Card>
                     </Collapse>
                 </div>
-            <h1>Métodos de envío</h1>
+            <h1 className="titulo-faq">Métodos de envío</h1>
                 <Button onClick={() => this.toggle(5)} className="button-Faq" >¿Cuáles son los métodos de envío o retiro disponibles?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.five.isOpen}>
                         <Card className="card-faq">
-                            <CardBody style={{width: '100%'}}>
-                                <p> El envio a domicilio tiene un costo estimado para envios en capital federal y otro para el interior del pais. 
+                            <CardBody className="card-body">
+                                <p> El envio a domicilio tiene un costo estimado para envios en capital federal y otro para el interior del país. 
                                     El retiro personal por nuestro local no tiene costos extras, 
-                                    aú así se encuientra temporalmente suspendido todo retiro de forma personal.</p>
+                                    aún así se encuientra temporalmente suspendido todo retiro de forma personal.</p>
                             </CardBody>
                         </Card>
                     </Collapse>
                     <Button onClick={() => this.toggle(6)} className="button-Faq" >¿Qué documentación se requiere para recibir o retirar mi compra?</Button>
                     <Collapse style={{transition: 'all 0.3s'}} isOpen={this.state.six.isOpen}>
                         <Card className="card-faq">
-                            <CardBody>
+                            <CardBody className="card-body">
                                 <p> Por razones de sanidad en debida conformidad comunitaria, 
                                     nuestra actividad no se encuentra autorizada a reanudar, por lo tanto, 
                                     quedan suspendidos los retiros en nuestro local. En el caso de que se 
@@ -125,6 +127,8 @@ class Faq extends React.Component {
                             </CardBody>
                         </Card>
                     </Collapse>
+                    </div>
+                    <Footer />
             </>
         )
     }
