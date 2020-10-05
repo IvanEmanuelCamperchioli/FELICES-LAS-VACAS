9dccbca26bebadb2494f546b859a3dc650123ed7
EddyVega-dev <evegag@fi.uba.ar>
2020-10-05 13:43:00 -0300

    arreglo del editUser

--- PyBackend/app.py
+++ PyBackend/app.py
@@ -8,23 +8,24 @@ CORS(app)
 def api():
     value = [
         "Buenos Aires","Catamarca","Chaco","Chubut","Córdoba",
-"Corrientes",
-"Entre Ríos",
-"Formosa",
-"Jujuy",
-"La Pampa",
-"La Rioja",
-"Mendoza",
-"Misiones",
-"Neuquén",
-"Río Negro",
-"Salta",
-"San Juan",
-"Santa Cruz",
-"Santa Fe",
-"Santiago del Estero",
-"Tierra del Fuego",
-"Tucumán"]
+        "Corrientes",
+        "Entre Ríos",
+        "Formosa",
+        "Jujuy",
+        "La Pampa",
+        "La Rioja",
+        "Mendoza",
+        "Misiones",
+        "Neuquén",
+        "Río Negro",
+        "Salta",
+        "San Juan",
+        "Santa Cruz",
+        "Santa Fe",
+        "Santiago del Estero",
+        "Tierra del Fuego",
+        "Tucumán"
+    ]
     
     return jsonify(value)
 
